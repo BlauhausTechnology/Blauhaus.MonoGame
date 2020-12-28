6a077bcbff3e41b59efadbf8d7fe625d764d2175
Adrian Frielinghaus <adrian.frielinghaus@gmail.com>
2020-12-28 07:16:44 +0200

    no message

---
+++ src/Blauhaus.MonoGame/Scenes/IScene.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Blauhaus.MonoGame.Scenes
+{
+    public interface IScene
+    {
+        public void Initialize();
+        public void LoadContent();
+
+        public void Update(GameTime gameTime);
+
+        public void BeforeDraw(SpriteBatch spriteBatch, Color clearColor);
+        public void Draw(SpriteBatch spriteBatch);
+        public void AfterDraw(SpriteBatch spriteBatch);
+
+        public void UnloadContent();
+        
+    }
+}
